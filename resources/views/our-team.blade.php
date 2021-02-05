9467ab913616a04fb94c3cca52ade7cca9f2e425
Terry Smith <tsmith@pipusa.com>
2021-02-05 08:33:37 -0500

    Updated Our Team styles and content

--- resources/views/our-team.blade.php
+++ resources/views/our-team.blade.php
@@ -2,9 +2,75 @@
 @section('title', 'STE | Our Team')
 
 @section('content')
-<div class="tile tile-dark-blue">
+<div class="tile tile-red">
     <h1 class="text-center">Our Team</h1>
-    <p class="lead text-center">content</p>
+    <div class="team-container">
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Tom Fascio (President)</div>
+                <div class="member-bio">
+                    <p>A US Army veteran who served six years, including a tour in Germany, Tom has been a member of the American Legion for 8 years; he is a former Vice Commander of Shelton Post 16, Shelton, Ct, and he is currently serving on the Honor Guard in Post 103 in Punta Gorda, Florida. He brings decades of advocacy experience, and has recently retired from a 28 year career, starting as a field representative/organizer, then becoming a staff representative, representing union members within the state of Connecticut.</p>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Diane Mitchell (Founder/Executive Director)</div>
+                <div class="member-bio">
+                    <p>Diane is the founder of Strength Through Education, Inc.  She is the proud mom of three boys, two of whom are actively serving in the military, US Army and US Air Force.  They are continuing a long, proud history of military service that touches nearly every generation in their family, and that can be traced to the Revolutionary War.  Diane is a lifelong educator.  She has held New York State teaching certification in High School English, Elementary 1-6, Elementary Special Education, and Teaching English to Speakers of Other Languages.  She left a teaching career that spanned 25 years in private and public schools to launch Strength Through Education, Inc.</p>
+                    <p> This organization allows her to continue following her passion for education, with a shift in focus from the classroom to the digital world.  Based on the personal experiences of her sons and their military buddies, Diane realized there is a desperate need for an educational platform providing information and support to service members through the various forms of digital medium most commonly used by this generation.  Her experience as a teacher has equipped her with the tools necessary to conduct thorough research, summarize and annotate information into a comprehensible format, and present it in a way that empowers the receiver with clear direction moving forward.</p>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Bette Rogers (Treasurer)</div>
+                <div class="member-bio">
+                    <p>Bette is a retired educator whose family has a long history of military service.  She is passionate about making sure service members and their families have a positive experience in the military and support when needed.</p>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Shelley Smith (Director of Operations)</div>
+                <div class="member-bio">
+                    <p>Bio content</p>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Gavin Goodman  (Marketing and Outreach Director)</div>
+                <div class="member-bio">
+                    <p>Gavin, is the owner of Southern Hospitality Experiences, located in Huntsville, Alabama.  It is a marketing, consulting, and event management company.  He has provided us with professional expertise (consulting services and help with our public launch) pro bono, to help get this organization off the ground.  He has also connected us with numerous military charities, professional affiliations, and legal counsel.</p>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Michael Smith (Videographer)</div>
+                <div class="member-bio">
+                    <p>Michael brings with him a deep passion for all things creative.  A recent graduate, Michael strives for excellence in every creative endeavor and is an accomplished musician and songwriter.</p>
+                </div>
+            </div>
+        </div>
+        <div class="team-member-card">
+            <div class="member-image"><img src="https://place-hold.it/150x225.jpg" alt=""></div>
+            <div class="member-content">
+                <div class="member-name">Terry Smith (Web Developer)</div>
+                <div class="member-bio">
+                    <p>Terry comes to Strength Through Education with 20 years of web development and application development experience.  He has a Bachelor's Degree in Graphic Design and a passion for visual arts.  Terry is a life-long supporter of the US military and many of his family members have served proudly.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
     
 @endsection
